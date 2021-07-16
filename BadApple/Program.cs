d3b96845efef95e900b4b24d46410c8a7206db4f
Fabian <lindgrenfabian@gmail.com>
2021-07-17 01:59:15 +0200

    add method to extract frames

--- BadApple/Program.cs
+++ BadApple/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace BadApple
 {
@@ -6,7 +8,23 @@ namespace BadApple
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(!File.Exists("frames/1.jpg"))
+            {
+                Console.Write("Video path: ");
+                string cmd = $"/C ffmpeg -i \"{Console.ReadLine()}\" -r 15/1 frames/%01d.jpg";
+                Process.Start("CMD.exe", cmd);
+            }
+            else
+            {
+                Console.WriteLine("Using already found frames.");
+            }
+
+
+        }
+
+        static void CreateFrames()
+        {
+
         }
     }
 }
