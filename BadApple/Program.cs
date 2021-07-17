b4f2870b20d30cbba54803a20a3f05f500eb7736
Fabian <lindgrenfabian@gmail.com>
2021-07-17 03:55:16 +0200

    create video out of frames

--- BadApple/Program.cs
+++ BadApple/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 
 namespace BadApple
@@ -9,10 +10,12 @@ namespace BadApple
     {
         private static void Main(string[] args)
         {
-            if (!File.Exists("frames/1.jpg"))
+            Console.Write("Video path: ");
+            string video = Console.ReadLine();
+            Directory.CreateDirectory("frames");
+            if (!File.Exists("frames/001.jpg"))
             {
-                Console.Write("Video path: ");
-                string cmd = $"/C ffmpeg -i \"{Console.ReadLine()}\" -r 15/1 -vf scale=1920:-1 frames/%01d.jpg";
+                string cmd = $"/C ffmpeg -i \"{video}\" -r 15/1 -vf scale=1920:-1 frames/%03d.jpg";
                 Process p = Process.Start("CMD.exe", cmd);
                 p.WaitForExit();
             }
@@ -20,10 +23,11 @@ namespace BadApple
             {
                 Console.WriteLine("Using already found frames.");
             }
-            CreateFrames();
+
+            CreateFrames(video);
         }
 
-        private static void CreateFrames()
+        private static void CreateFrames(string video)
         {
             Console.Write("Image path: ");
             string source = Console.ReadLine();
@@ -32,22 +36,64 @@ namespace BadApple
                 Console.WriteLine("Image does not exist.");
                 return;
             }
-
             //resize img
             Bitmap sourceBitmap = new Bitmap(48, 48);
             using (Graphics g = Graphics.FromImage(sourceBitmap)) g.DrawImage(new Bitmap(source), 0, 0, 48, 48);
 
+            Bitmap[] sources = new Bitmap[101];
+
             //generate frames
             foreach (string path in Directory.GetFiles("frames", "*.jpg"))
             {
+                string newPath = path.Replace("frames", "output");
+                if (File.Exists(newPath)) continue;
                 Bitmap frame = new Bitmap(path);
+
+                //create black bitmap
                 Bitmap newFrame = new Bitmap(frame.Width, frame.Height);
+                Graphics gr = Graphics.FromImage(newFrame);
+                gr.Clear(Color.Black);
 
-                using (Graphics g = Graphics.FromImage(frame))
+                using (Graphics g = Graphics.FromImage(newFrame))
                 {
+                    for (int y = 0; y < newFrame.Height; y += 48)
+                    {
+                        for (int x = 0; x < newFrame.Width; x += 48)
+                        {
+                            //crop area and get avg color
+                            Rectangle area = new Rectangle(new Point(x, y), new Size(48, 48));
+                            Bitmap crop = ImageProcessing.CropImage(frame, area);
+                            double trans = 1 - Math.Round(ImageProcessing.CalculateAverageLightness(crop), 2);
 
+                            //get source image with correct transparency
+                            Bitmap sourceTrans = sources[Convert.ToInt32(trans * 100)];
+
+                            //create source image if not exists
+                            if (sourceTrans == null)
+                            {
+                                sources[Convert.ToInt32(trans * 100)] = sourceTrans = (Bitmap)ImageProcessing.ChangeImageOpacity(sourceBitmap, trans);
+                                Console.WriteLine($"Created image with transparency level {trans}");
+                            }
+
+                            //place source image
+                            g.DrawImage(sourceTrans, new Point(x, y));
+                        }
+                    }
+                    Directory.CreateDirectory(Path.GetDirectoryName(newPath));
+                    newFrame.Save(newPath, ImageFormat.Jpeg);
+                    Console.WriteLine($"{path} created.");
                 }
             }
+            Console.WriteLine("Processed all frames. Generating audio.");
+            Process processAudio = Process.Start("CMD.exe", $"/C ffmpeg -i {video} -y output/audio.mp3");
+            processAudio.WaitForExit();
+
+            Console.WriteLine("Generating video.");
+            Process processVideo = Process.Start("CMD.exe", $"/C ffmpeg -r 15 -i output/%03d.jpg -i output/audio.mp3 -c:v libx264 -c:a aac -pix_fmt yuv420p -crf 23 -r 15 -shortest -y output.mp4");
+            processVideo.WaitForExit();
+
+            Process.Start("CMD.exe", $"/C \"{Directory.GetCurrentDirectory()}\\output.mp4\"");
+            Console.WriteLine("Done uwu");
         }
     }
 }
